0bf60ed68638b1da03e2a4ea202cc1e4af3bc016
PawelOchal <opawel262@gmail.com>
2023-07-31 19:41:54 +0200

    added edit feature

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('addTask/',views.addTask,name='addTask'),
+    path('add_task/',views.add_task,name='add_task'),
+    path('mark_as_completed/<int:pk>/',views.mark_as_completed,name='mark_as_completed'),
+    path('mark_as_uncompleted/<int:pk>/',views.mark_as_uncompleted,name='mark_as_uncompleted'),
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+
 
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -4,13 +4,39 @@ from .models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+
     context = {
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
     }
     return render(request, 'home.html', context)
 
-def addTask(request):
+def add_task(request):
     if request.method == 'POST':
         task = request.POST['task']
         Task.objects.create(task=task)
     return redirect('home')
+
+def mark_as_completed(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_uncompleted(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == 'POST':
+        task.task = request.POST['task']
+        task.save()
+        return redirect('home')
+    context = {
+        'task':task,
+    }
+    return render(request, 'edit_task.html', context)
